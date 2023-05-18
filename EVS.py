ab2ec586e70e07d1122963f30c040ce69849f232
v000e <102678767+vvsne@users.noreply.github.com>
2023-05-18 07:32:29 +0300

    Update EVS.py

--- EVS.py
+++ EVS.py
@@ -1,14 +1,17 @@
 import requests
-import The_main
+from The_main import *
 
-def createEVSDisk(endpoint, current_project, TOKEN):
-    url = 'https://{endpoint}/v2/{project_id}/volumes'.format(endpoint=endpoint, project_id=current_project)
-    name = "ft"
-    size = 30
-    az = "ru-moscow-1b"
-    count = 1
-    description = "new evs az2"
-    volumeType = "SSD"
+TOKEN = token()
+
+name = "ft"
+size = 30
+az = "ru-moscow-1b"
+count = 1
+description = "new evs az2"
+volumeType = "SSD"
+
+def createEVSDisk(current_project, TOKEN, name, size, az, count, description, volumeType):
+    url = 'https://evs.ru-moscow-1.hc.sbercloud.ru/v2/{project_id}/volumes'.format(project_id=current_project)
     header = {'Content-Type': 'application/json', 'X-Auth-Token': TOKEN}
     json = {
         "volume": {
@@ -16,19 +19,19 @@ def createEVSDisk(endpoint, current_project, TOKEN):
             # "snapshot_id": null,   в целом важные параметры, но не уверен, что это всё будет прописано в итоге
             # "imageRef": null,
             "count": count,
-            "description": "new evs az2",
-            "availability_zone": "ru-moscow-1b",  # not requested from user
-            "size": 12,
-            "name": "ft",
-            "volume_type": "SSD"
+            "description": "{description}".format(description = description),
+            "availability_zone": "{az}".format(az = az),  # not requested from user
+            "size": size,
+            "name": "{name}".format(name = name),
+            "volume_type": "{vt}".format(vt = volumeType)
         }
     }
     createDisk = requests.post(url, headers=header, json=json)
     return createDisk
 
 
-def listing_volumes_of_project_api(endpoint, current_project, TOKEN):  # подробная информация обо всех машинах
-    url = 'https://{endpoint}/v2/{project_id}/volumes/detail'.format(endpoint=endpoint, project_id=current_project)
+def listing_volumes_of_project_api(current_project, TOKEN):  # подробная информация обо всех машинах
+    url = 'https://evs.ru-moscow-1.hc.sbercloud.ru/v2/{project_id}/volumes/detail'.format(project_id=current_project)
     header = {'Content-Type': 'application/json', 'X-Auth-Token': TOKEN}
     listing_volumes_of_project_api = requests.get(url, headers=header)
     return listing_volumes_of_project_api
@@ -41,13 +44,13 @@ def volumes_id(current_project):
     return volumes_id
 
 
-def getvolumeIdByName(current_project, name):
-    info = getDiskInformationByName(current_project, name)
+def getvolumeIdByName(current_project, name, TOKEN):
+    info = getDiskInformationByName(current_project, name, TOKEN)
     return info['id']
 
 
 def deleteEVSDisk(current_project, name, TOKEN):
-    current_volume = getvolumeIdByName(current_project, name)
+    current_volume = getvolumeIdByName(current_project, name, TOKEN)
     url = 'https://evs.ru-moscow-1.hc.sbercloud.ru/v2/{project_id}/volumes/{volume_id}'.format(
         project_id=current_project, volume_id=current_volume)
     header = {'Content-Type': 'application/json', 'X-Auth-Token': TOKEN}
@@ -55,8 +58,8 @@ def deleteEVSDisk(current_project, name, TOKEN):
     return deleteDisk
 
 
-def getDiskInformationByName(current_project, name):
-    allDisks = listing_volumes_of_project_api(current_project)
+def getDiskInformationByName(current_project, name, TOKEN):
+    allDisks = listing_volumes_of_project_api(current_project, TOKEN)
     diskNames = []
     for i in range(len(allDisks.json()['volumes'])):
         diskNames.append(allDisks.json()['volumes'][i]['name'])
@@ -65,16 +68,18 @@ def getDiskInformationByName(current_project, name):
             return allDisks.json()['volumes'][i]
     return "No disk with such name"
 
+nameUp = "test-put"
+descUp = "test"
 
-def updateInfo(current_project, name, TOKEN):
-    current_volume = getvolumeIdByName(current_project, name)
+def updateInfo(current_project, name, TOKEN, newName, newDescription):
+    current_volume = getvolumeIdByName(current_project, name, TOKEN)
     url = 'https://evs.ru-moscow-1.hc.sbercloud.ru/v2/{project_id}/volumes/{volume_id}'.format(
         project_id=current_project, volume_id=current_volume)
     header = {'Content-Type': 'application/json', 'X-Auth-Token': TOKEN}
     json = {
         "volume": {
-            "name": "test_put",
-            "description": "test"  # запрашиваются
+            "name": "{name}".format(name = newName),
+            "description": "{desc}".format(desc = newDescription)  # запрашиваются
         }
     }
     updateDisk = requests.put(url, headers=header, json=json)
